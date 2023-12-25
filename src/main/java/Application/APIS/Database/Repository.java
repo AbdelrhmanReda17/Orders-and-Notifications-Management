f055e5e742223fa3d225215c2098026aa21f4100
YassinTarekHelmy <11410120210450@stud.cu.edu.eg>
2023-12-25 19:13:03 +0200

    Database Repository is finished

--- src/main/java/Application/APIS/Database/Repository.java
+++ src/main/java/Application/APIS/Database/Repository.java
@@ -12,10 +12,4 @@ public interface Repository<T , S> {
     public boolean existsById(S id);
     public void deleteById(S id);
     public void deleteAll();
-    public long count();
-    public void copy(T object);
-    public void saveAll(T[] objects);
-    public void updateAll(T[] objects);
-    public void deleteAll(T[] objects);
-    public void deleteByIds(S[] ids);
 }
---
+++ src/main/java/Application/APIS/Notifications/Service/NotificationService.java
@@ -0,0 +1,8 @@
+package Application.APIS.Notifications.Service;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class NotificationService {
+
+}
